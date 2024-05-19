30887ef4decf5283ebff8d43accb7079e85595f6
Aniket251996 <ronitkumar1222@gmail.com>
2024-05-19 12:46:34 +0530

    Add files via upload

---
+++ Youtube_Streamlit_1.py
@@ -0,0 +1,233 @@
+# Streamlit App Developement
+import streamlit as st
+import mysql.connector
+import re
+
+# Function to execute SQL query and display results based on user selection
+def execute_query(option):
+    mydb = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='Root',
+        database='project'
+    )
+    cursor = mydb.cursor()
+#question 1
+    if option == "What are the names of all the videos and their corresponding channels?":
+        query = """select v.vedio_name,c.channel_name from project.video as v
+                    inner join project.channel as c on v.channel_id = c.channel_id 
+                """
+        
+        cursor.execute(query)
+        results = cursor.fetchall()
+        st.write("What are the names of all the videos and their corresponding channels?")
+        if results:
+            columns = ["Video Name", "Channel Name"]
+            results = [(str(row[0]), str(row[1])) for row in results]
+            st.table([columns] + results)
+        else:
+            st.write("No results found.")
+#question2    
+    if option == "Which channels have the most number of videos, and how many videos do they have?":
+        query = """SELECT v.channel_id, c.channel_name, COUNT(vedio_name) AS video_count 
+                FROM video as v
+                INNER JOIN channel as c  ON v.channel_id = c.channel_id 
+                GROUP BY v.channel_id ,c.channel_name
+                ORDER BY video_count DESC 
+                limit 1
+                """
+        cursor.execute(query)
+        results = cursor.fetchall()
+        st.write("Which channels have the most number of videos, and how many videos do they have?")
+        if results:
+            columns = ["Channel ID", "Channel Name", "Video Count"]
+            results = [(str(row[0]), str(row[1]), str(row[2])) for row in results]
+            st.table([columns] + results)
+        else:
+            st.write("No results found.")
+#question3
+    if option == "What are the top 10 most viewed videos and their respective channels?":
+        query = """select v.vedio_name,v.view_count, c.channel_name from video as v
+                    inner join channel as c on v.channel_id = c.channel_id 
+                    order by view_count desc
+                    limit 10;
+                """
+        cursor.execute(query)
+        results = cursor.fetchall()
+        st.write("What are the top 10 most viewed videos and their respective channels?")
+        if results:
+            columns = ["video name","No of view", "Channel Name"]
+            results = [(str(row[0]), str(row[1]),str(row[2])) for row in results]
+            st.table([columns] + results)
+        else:
+            st.write("No results found.")
+#queation 4
+    if option == "How many comments were made on each video, and what are their corresponding video names?":
+        query = """select count(c.comment_id) as comment_count,c.video_id as video_id,v.vedio_name as video_name from project.comment as c
+                    inner join project.video as v on v.video_id = c.video_id
+                    group by c.video_id,v.vedio_name;
+                """
+        cursor.execute(query)
+        results = cursor.fetchall()
+        st.write("How many comments were made on each video, and what are their corresponding video names?")
+        if results:
+            columns = ["Comment Count","Video ID", "Video Name"]
+            results = [(str(row[0]), str(row[1]),str(row[2])) for row in results]
+            st.table([columns] + results)
+        else:
+            st.write("No results found.")
+#queation 5
+    if option == "Which videos have the highest number of likes, and what are their corresponding channel names?":
+        query = """select v.vedio_name as video_name , c.channel_name as channel_name from video as v
+                    inner join channel as c on v.channel_id = v.channel_id
+                    order by like_count desc
+                    limit 1;
+                """
+        cursor.execute(query)
+        results = cursor.fetchall()
+        st.write("Which videos have the highest number of likes, and what are their corresponding channel names?")
+        if results:
+            columns = ["Video Name", "Channel Name"]
+            results = [(str(row[0]), str(row[1])) for row in results]
+            st.table([columns] + results)
+        else:
+            st.write("No results found.")
+#question 6
+    if option == "What is the total number of likes and dislikes for each video, and what are their corresponding video names?":
+        query = """select vedio_name as video_name , like_count as No_of_like from video
+                """
+        cursor.execute(query)
+        results = cursor.fetchall()
+        st.write("What is the total number of likes and dislikes for each video, and what are their corresponding video names?")
+        if results:
+            columns = ["Video Name", "Likes"]
+            results = [(str(row[0]), str(row[1])) for row in results]
+            st.table([columns] + results)
+        else:
+            st.write("No results found.")    
+# Question 7
+    if option == "What is the total number of views for each channel, and what are their corresponding channel names?":
+        query = """select channel_name as Channel_name, channel_view as channel_views from channel
+                """
+        cursor.execute(query)
+        results = cursor.fetchall()
+        st.write("What is the total number of views for each channel, and what are their corresponding channel names?")
+        if results:
+            columns = ["channel Name", "Channel View"]
+            results = [(str(row[0]), str(row[1])) for row in results]
+            st.table([columns] + results)
+        else:
+            st.write("No results found.") 
+# Question 8
+    if option == "What are the names of all the channels that have published videos in the year 2022?":
+        query = """select c.channel_name,v.vedio_name from video as v
+                    inner join channel as c on c.channel_id = v.channel_id
+                    where v.video_publishedat like '2022%'
+                """
+        cursor.execute(query)
+        results = cursor.fetchall()
+        st.write("What are the names of all the channels that have published videos in the year 2022?")
+        if results:
+            columns = ["Channel Name","Video Name"]
+            results = [(str(row[0]),str(row[1])) for row in results]
+            st.table([columns] + results)
+        else:
+            st.write("No results found.") 
+# Question 9
+    if option == "What is the average duration of all videos in each channel, and what are their corresponding channel names?":
+
+        query = """SELECT 
+                channel_id,
+                channel_name,
+                SUM(extracted_data) AS total_extracted_data
+            FROM (
+                SELECT 
+                    v.channel_id,
+                    c.channel_name,
+                    ROUND(AVG(
+                        CASE 
+                            WHEN INSTR(v.duration, 'M') > 0 
+                            THEN SUBSTRING_INDEX(SUBSTRING_INDEX(v.duration, 'PT', -1), 'M', 1)
+                            ELSE 0 
+                        END), 2) AS extracted_data
+                FROM project.video AS v
+                INNER JOIN project.channel AS c ON v.channel_id = c.channel_id
+                GROUP BY v.channel_id, c.channel_name
+                
+                UNION ALL
+                
+                SELECT 
+                    v2.channel_id,
+                    c2.channel_name,
+                    ROUND((AVG(
+                        CASE 
+                            WHEN v2.duration LIKE 'PT%M%S' THEN 
+                                SUBSTRING_INDEX(SUBSTRING_INDEX(v2.duration, 'M', -1), 'S', 1)
+                            WHEN v2.duration LIKE 'PT%S' THEN
+                                SUBSTRING_INDEX(SUBSTRING_INDEX(v2.duration, 'PT', -1), 'S', 1)
+                            ELSE 
+                                0
+                        END ) / 60), 2) AS extracted_data
+                FROM project.video AS v2
+                INNER JOIN project.channel AS c2 ON v2.channel_id = c2.channel_id
+                GROUP BY v2.channel_id, c2.channel_name
+            ) AS subquery
+            GROUP BY channel_id, channel_name;
+        """
+  
+        cursor.execute(query)
+        results = cursor.fetchall()
+        
+        st.write("What is the average duration of all videos in each channel, and what are their corresponding channel names?")
+        if results:
+            columns = ["Channel ID","Channel Name","Average Duration in Min"]
+            results = [(str(row[0]),str(row[1]),str(row[2])) for row in results]
+            st.table([columns] + results)
+        else:
+            st.write("No results found.")       
+# Question 10
+    if option == "Which videos have the highest number of comments, and what are their corresponding channel names?":
+        query = """SELECT v.vedio_name, c.channel_name
+                    FROM video AS v
+                    JOIN channel AS c ON v.channel_id = c.channel_id
+                    JOIN (
+                        SELECT c.video_id, COUNT(c.comment_id) AS comment_count
+                        FROM comment AS c
+                        GROUP BY c.video_id
+                    ) AS comment_counts
+                    ON v.video_id = comment_counts.video_id
+                    ORDER BY comment_counts.comment_count DESC
+                    LIMIT 1;
+                """
+        cursor.execute(query)
+        results = cursor.fetchall()
+        st.write("Videos with the highest number of comments and their corresponding channel names:")
+        if results:
+            columns = ["Video Name", "Channel Name"]
+            results = [(str(row[0]), str(row[1])) for row in results]
+            st.table([columns] + results)
+        else:
+            st.write("No results found.")
+
+
+# Streamlit App Title
+st.title(':blue[Youtube API Project]')
+#channel_id = st.text_input("Enter your channel ID")
+
+option = st.selectbox(
+    "Please select the question?",
+    ("What are the names of all the videos and their corresponding channels?",
+    "Which channels have the most number of videos, and how many videos do they have?",
+    "What are the top 10 most viewed videos and their respective channels?",
+    "How many comments were made on each video, and what are their corresponding video names?",
+    "Which videos have the highest number of likes, and what are their corresponding channel names?",
+    "What is the total number of likes and dislikes for each video, and what are their corresponding video names?",
+    "What is the total number of views for each channel, and what are their corresponding channel names?",
+    "What are the names of all the channels that have published videos in the year 2022?",
+    "What is the average duration of all videos in each channel, and what are their corresponding channel names?",
+    "Which videos have the highest number of comments, and what are their corresponding channel names?"),
+    index=None)
+    
+# Streamlit Run button
+if st.button('Run Query'):
+    execute_query(option)
---
+++ youtube_data_extraction.py
@@ -0,0 +1,258 @@
+"""
+# If we need to pull data from youtube API
+import googleapiclient.discovery
+api_service_name = "youtube"
+api_version = "v3"
+api_key = "AIzaSyAa0fPsrlLjqflao9ovCUHoPLUP1I6d3XU"
+youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey = api_key)
+c_id ="UCtGbExCzlwmsyWKpxLnyEww"
+
+#Pull Channel Data
+def channel_data(c_id):
+     request = youtube.channels().list(
+        part="snippet,contentDetails,statistics",
+        id= c_id
+    )
+     response = request.execute()
+
+     data = {"channel_id" : response['items'][0]['id'],
+             "channel_name" : response['items'][0]['snippet']['title'],
+             "channel_view" : response['items'][0]['statistics']['viewCount'],
+             "channel_description" : response['items'][0]['snippet']['description'],
+             "channel_publishedAt" : response['items'][0]['snippet']['publishedAt'],
+             "playlist_id" : response['items'][0]['contentDetails']['relatedPlaylists']['uploads'],
+             "subscription_count" : response['items'][0]['statistics']['subscriberCount']
+             }
+     return data
+        
+#pull Playlist data
+
+def playlist_data(c_id):
+     request = youtube.playlists().list(
+        part="snippet",
+        channelId=c_id
+        )
+     response = request.execute()
+
+     data ={ "playlist_id":response['items'][0]['id'],
+        "channel_id" : response['items'][0]['snippet']['channelId'],
+        "playlist_name":response['items'][0]['snippet']['title']
+           }
+
+#Pull video data
+search_request = youtube.search().list(
+    part="id",
+    channelId=c_id,
+    type="video"
+    )
+search_response = search_request.execute()
+
+# Extract video IDs from the search results
+video_ids = [item['id']['videoId'] for item in search_response['items']]
+
+# Use the video IDs to retrieve detailed information about each video
+videos_request = youtube.videos().list(
+    part="id,snippet,status,statistics,contentDetails",
+    id=','.join(video_ids)
+    )
+videos_response = videos_request.execute()
+
+def videos_data(c_id):
+    video_table = []
+    for item in videos_response['items']:
+        video_data = {
+            "video_id": item['id'],
+            "channel_id": item['snippet']['channelId'],
+            "video_name": item['snippet']['title'],
+            "video_description": item['snippet']['description'],
+            "video_publishedat": item['snippet']['publishedAt'],
+            "view_count": item['statistics']['viewCount'],
+            "like_count": item['statistics']['likeCount'],
+            "favoriteCount": item['statistics']['favoriteCount'],
+            "commentCount": item['statistics']['commentCount'],
+            "duration": item['contentDetails']['duration'],
+            "default_thumbnails": item['snippet']['thumbnails']['default']['url'],
+            "caption_status": item['contentDetails']['caption']
+        }
+        video_table.append(video_data)
+    return video_table
+
+# Pull Comment data for the respective video
+comment_table = []
+for video_id in video_ids:
+    request = youtube.commentThreads().list(
+        part="id,snippet",
+        videoId=video_id
+    )
+    response = request.execute()
+    if 'items' in response:
+        for item in response['items']:
+            comment_table.append({
+                'comment_id': item['id'],
+                'video_id': item['snippet']['videoId'],
+                'comment_text':item['snippet']['topLevelComment']['snippet']['textOriginal'],
+                'author_name':item['snippet']['topLevelComment']['snippet']['authorDisplayName'],
+                'comment_publishedAt':item['snippet']['topLevelComment']['snippet']['publishedAt']
+            })
+
+
+# To connect SQl with python
+import mysql.connector
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password ='Root',
+    database= 'project'
+)  
+
+cursor= mydb.cursor()
+
+#Create a table in SQL with python Query
+# Define the SQL query to create the Channel table
+create_table_query = '''
+    CREATE TABLE IF NOT EXISTS Channel (
+        channel_id VARCHAR(255),
+        channel_name VARCHAR(255),
+        channel_view INT,
+        channel_description TEXT,
+        playlist_id VARCHAR(255),
+        subscription_count INT,
+        channel_publishedAt varchar(255)
+    )
+  '''
+# Execute the create table query
+cursor.execute(create_table_query)
+
+# Define the SQL query to create the playlist table
+playlist_table = '''
+    CREATE TABLE IF NOT EXISTS Playlist (
+        playlist_id varchar(255),
+        channel_id varchar(255),
+        playlist_name varchar(255)
+        )
+        '''
+# Execute the create table query
+cursor.execute(playlist_table)
+
+# Define the SQL query to create the video table
+video_table_query = '''
+    CREATE TABLE IF NOT EXISTS Video (
+        video_id varchar(255),
+        channel_id varchar(255),
+        vedio_name varchar(255),
+        video_description text,
+        video_publishedat varchar(255),
+        view_count int,
+        like_count int,
+        favoriteCount int,
+        commentCount int,
+        duration varchar(255),
+        default_thumbnails varchar(255),
+        caption_status varchar(255)
+        )
+    '''
+# Execute the create table query
+cursor.execute(video_table_query)
+
+# Define the SQL query to create the comment table
+comment_table_query = '''
+    CREATE TABLE IF NOT EXISTS comment (
+        comment_id varchar(255),
+        video_id varchar(255),
+        comment_text text,
+        author_name varchar(255),
+        comment_publishedAt varchar(255)
+        )
+     '''
+# Execute the create table query
+cursor.execute(comment_table_query)
+
+
+# Insert Data in table
+# Define the insert query for channel table
+insert_query = "INSERT INTO Channel VALUES (%s, %s, %s, %s, %s, %s, %s)"
+
+# Get channel data
+channel_data = channel_data(c_id)
+
+data = (
+    channel_data['channel_id'],
+    channel_data['channel_name'],
+    channel_data['channel_view'],
+    channel_data['channel_description'],
+    channel_data['playlist_id'],
+    channel_data['subscription_count'],
+    channel_data['channel_publishedAt']
+)
+
+# Execute the insert query with data
+cursor.execute(insert_query, data)
+mydb.commit()
+
+# Define the insert query for Playlist Table
+insert_query = "INSERT INTO Playlist VALUES (%s, %s, %s)"
+
+# Get channel data
+playlist_data = playlist_data(c_id)
+
+data = (
+    playlist_data['playlist_id'],
+    playlist_data['channel_id'],
+    playlist_data['playlist_name']
+)
+
+# Execute the insert query with data
+cursor.execute(insert_query, data)
+mydb.commit()
+
+# Define the insert query into video table
+insert_query = '''
+    INSERT INTO Video 
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    '''
+
+# Get channel data
+video_data = videos_data(c_id)
+
+for i in range(len(video_data)):
+    data = (
+        video_data[i]['video_id'],
+        video_data[i]['channel_id'],
+        video_data[i]['video_name'],
+        video_data[i]['video_description'],
+        video_data[i]['video_publishedat'],
+        video_data[i]['view_count'],
+        video_data[i]['like_count'],
+        video_data[i]['favoriteCount'],
+        video_data[i]['commentCount'],
+        video_data[i]['duration'],
+        video_data[i]['default_thumbnails'],
+        video_data[i]['caption_status']
+        )
+    
+        # Execute the insert query with data
+    cursor.execute(insert_query, data)
+    mydb.commit()
+
+# Define the insert query into comment
+insert_query = '''
+    INSERT INTO comment 
+    VALUES (%s, %s, %s, %s, %s)
+'''
+
+# Get channel data
+comment_data = comment_table
+
+for i in range(len(comment_data)):
+    data = (
+        comment_data[i]['comment_id'],
+        comment_data[i]['video_id'],
+        comment_data[i]['comment_text'],
+        comment_data[i]['author_name'],
+        comment_data[i]['comment_publishedAt']
+     )
+    
+    # Execute the insert query with data
+    cursor.execute(insert_query, data)
+    mydb.commit()
+"""
